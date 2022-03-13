2037d75f8a713f5763140a50c268f351297a1041
komo <66017773+komo135@users.noreply.github.com>
2022-03-14 05:10:28 +0900

    Add files via upload

--- github_app.py
+++ github_app.py
@@ -1,8 +1,136 @@
 import streamlit as st
-from agent import dqn, qrdqn
 from traderl import data, nn
 import pandas as pd
 import gc
+
+from traderl import agent
+import numpy as np
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+
+
+class DQN(agent.DQN):
+    def evolute(self, h, h_):
+        pips, profits, total_pips, total_profits, total_pip, total_profit, buy, sell = self.trade(h, h_)
+
+        acc = np.mean(pips > 0)
+        total_win = np.sum(pips[pips > 0])
+        total_lose = np.sum(pips[pips < 0])
+        rr = total_win / abs(total_lose)
+        ev = (np.mean(pips[pips > 0]) * acc + np.mean(pips[pips < 0]) * (1 - acc)) / abs(np.mean(pips[pips < 0]))
+
+        fig = plt.figure(figsize=(10, 5), dpi=100)
+        plt.subplot(1, 2, 1)
+        plt.plot(total_pips)
+        plt.subplot(1, 2, 2)
+        plt.plot(total_profits)
+        st.pyplot(fig)
+
+        st.write(
+            f"acc = {acc}, pips = {sum(pips)}\n"
+            f"total_win = {total_win}, total_lose = {total_lose}\n"
+            f"rr = {rr}, ev = {ev}\n"
+        )
+
+    def plot_trade(self, train=False, test=False, period=1):
+        assert train or test
+        h = 0
+        if test:
+            h = self.test_step[0]
+        elif train:
+            h = np.random.randint(0, int(self.train_step[-1] - 960 * period))
+        h_ = h + len(self.train_step) // 12 * period
+        trend = self.y[h:h_]
+
+        pips, profits, total_pips, total_profits, total_pip, total_profit, buy, sell = self.trade(h, h_)
+
+        fig = plt.figure(figsize=(20, 10), dpi=100)
+        plt.plot(trend, color="g", alpha=1, label="close")
+        plt.plot(trend, "^", markevery=buy, c="red", label='buy', alpha=0.7)
+        plt.plot(trend, "v", markevery=sell, c="blue", label='sell', alpha=0.7)
+
+        plt.legend()
+        st.pyplot(fig)
+
+        st.write(f"pip = {np.sum(pips)}"
+                 f"\naccount size = {total_profit}"
+                 f"\ngrowth rate = {total_profit / self.account_size}"
+                 f"\naccuracy = {np.mean(np.array(pips) > 0)}")
+
+    def plot_result(self, w, risk=0.1):
+        self.model.set_weights(w)
+        self.risk = risk
+
+        fig = plt.figure(figsize=(20, 5), dpi=100)
+        plt.subplot(1, 2, 1)
+        plt.plot(self.test_pip)
+        plt.subplot(1, 2, 2)
+        plt.plot(self.test_profit)
+        st.pyplot(fig)
+
+        ################################################################################
+        self.plot_trade(train=False, test=True, period=9)
+        ################################################################################
+        fig = plt.figure(figsize=(10, 5))
+        plt.plot(self.train_loss)
+        plt.plot(self.val_loss)
+        plt.title('Model loss')
+        plt.ylabel('Loss')
+        plt.xlabel('Epoch')
+        plt.legend(['Train', 'Validation'], loc='upper left')
+        st.pyplot(fig)
+
+        len_ = len(self.train_loss) // 2
+        fig = plt.figure(figsize=(10, 5))
+        plt.plot(self.train_loss[len_:])
+        plt.plot(self.val_loss[len_:])
+        plt.title('Model loss')
+        plt.ylabel('Loss')
+        plt.xlabel('Epoch')
+        plt.legend(['Train', 'Validation'], loc='upper left')
+        st.pyplot(fig)
+        ################################################################################
+        self.evolute(self.test_step[0], self.test_step[-1])
+        ################################################################################
+        fig = plt.figure(figsize=(20, 5), dpi=100)
+        plt.subplot(1, 2, 1)
+        plt.plot(self.train_rewards)
+        plt.subplot(1, 2, 2)
+        plt.plot(self.test_rewards)
+        st.pyplot(fig)
+
+        st.write(f"profits = {self.max_profit}, max profits = {self.max_profits}\n"
+                 f"pips = {self.max_pip}, max pip = {self.max_pips}")
+        ################################################################################
+        self.evolute(self.test_step[0] - len(self.train_step), self.test_step[0])
+
+    def train(self, epoch=40, batch_size=2056):
+        for _ in range(600 // epoch):
+            clear_output()
+            fig = plt.figure(figsize=(10, 5))
+            plt.plot(self.train_loss)
+            plt.plot(self.val_loss)
+            plt.title('Model loss')
+            plt.ylabel('Loss')
+            plt.xlabel('Epoch')
+            plt.legend(['Train', 'Validation'], loc='upper left')
+            st.pyplot(fig)
+            self._train(epoch, batch_size)
+            self.target_model.set_weights(self.model.get_weights())
+
+
+class QRDQN(agent.QRDQN, DQN):
+    def evolute(self, h, h_):
+        DQN.evolute(self, h, h_)
+
+    def plot_trade(self, train=False, test=False, period=1):
+        DQN.plot_trade(self, train, test, period)
+
+    def plot_result(self, w, risk=0.1):
+        DQN.plot_result(self, w, risk)
+
+    def train(self, epoch=50, batch_size=2056):
+        DQN.train(self, epoch, batch_size)
 
 
 class App:
@@ -21,7 +149,7 @@ class App:
 
         if select == "forex":
             symbol = col1.selectbox("", ("AUDJPY", "AUDUSD", "EURCHF", "EURGBP", "EURJPY", "EURUSD", "GBPJPY",
-                                                        "GBPUSD", "USDCAD", "USDCHF", "USDJPY", "XAUUSD"))
+                                         "GBPUSD", "USDCAD", "USDCHF", "USDJPY", "XAUUSD"))
             timeframe = col2.selectbox("", ("m15", "m30", "h1", "h4", "d1"))
             if load_file:
                 self.df = data.get_forex_data(symbol, timeframe)
@@ -56,7 +184,7 @@ class App:
             self.select_data()
 
     def create_agent(self, agent_name, args):
-        agent_dict = {"dqn": dqn.DQN, "qrdqn":qrdqn.QRDQN}
+        agent_dict = {"dqn": DQN, "qrdqn": QRDQN}
         self.agent = agent_dict[agent_name](**args)
 
     def agent_select(self):
@@ -96,7 +224,7 @@ class App:
             st.write("Agent created")
 
     def agent_train(self):
-        if self.agent:
+        if agent:
             if st.button("training"):
                 self.agent.train()
         else:
@@ -111,7 +239,7 @@ class App:
                        "please create agent")
 
     def model_save(self):
-        if self.agent:
+        if agent:
             save_name = st.text_input("save name", self.model_name)
             if st.button("model save"):
                 self.agent.model.save(save_name)
@@ -142,7 +270,7 @@ def home():
     md = """
     # Traderl Web Application
     This web app is intuitive to [traderl](https://github.com/komo135/trade-rl).
-    
+
     # How to Execute
     1. select data
         * Click on "select data" on the sidebar to choose your data.
